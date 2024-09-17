ad4b9aa8e9446624bb39ee1b0242f522fe0879ef
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-09-17 21:44:17 +0530

    style

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SelectionResult from "./Selection";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-#9b8282 flex items-center justify-center p-4">
+    <div className="min-h-screen bg-black flex items-center justify-center p-4">
       <SelectionResult />
     </div>
   );
